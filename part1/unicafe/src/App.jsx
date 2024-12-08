9834adde92f2bb75b7f05823996cc13c528e668b
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-12-08 10:40:38 -0500

    Step 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <form onSubmit={(e)=>(e.preventDefault())}>
+        <h1>Give Feedback</h1>
+        <button onClick={()=>{setGood(good+1)}}>Good</button>
+        <button onClick={()=>{setNeutral(neutral+1)}}>Neutral</button>
+        <button onClick={()=>{setBad(bad+1)}}>Bad</button>
+      </form>
+      <h2>Statistics</h2>
+      <h3>Good:</h3>
+      <p>{good}</p>
+      <h3>Neutral: </h3>
+      <p>{neutral}</p>
+      <h3>Bad: </h3>
+      <p>{bad}</p>
+    </>
+  )
+}
+
+export default App
